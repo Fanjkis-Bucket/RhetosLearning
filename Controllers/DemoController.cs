a7fe743d0ca2882b10ed85c14108cff62efc540f
Dominik Fanjkutić <dfanjkutic@omega-software.hr>
2022-09-05 15:59:49 +0200

    Action/Load/Query

--- Controllers/DemoController.cs
+++ Controllers/DemoController.cs
@@ -1,18 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bookstore;
+using Bookstore.Repositories;
+using Microsoft.AspNetCore.Mvc;
 using Rhetos;
+using Rhetos.Dom.DefaultConcepts;
+using Rhetos.Logging;
 using Rhetos.Processing;
 using Rhetos.Processing.DefaultCommands;
+using Rhetos.Utilities;
+using System.Collections;
 
 [Route("Demo/[action]")]
 public class DemoController : ControllerBase
 {
     private readonly IProcessingEngine processingEngine;
     private readonly IUnitOfWork unitOfWork;
+    private readonly Common.ExecutionContext _context;
 
-    public DemoController(IRhetosComponent<IProcessingEngine> processingEngine, IRhetosComponent<IUnitOfWork> unitOfWork)
+
+    public DemoController(IRhetosComponent<IProcessingEngine> processingEngine, IRhetosComponent<IUnitOfWork> unitOfWork, IRhetosComponent<Common.ExecutionContext> context)
     {
         this.processingEngine = processingEngine.Value;
         this.unitOfWork = unitOfWork.Value;
+        _context = context.Value;
+
     }
 
     [HttpGet]
@@ -20,6 +30,7 @@ public class DemoController : ControllerBase
     {
         var readCommandInfo = new ReadCommandInfo { DataSource = "Bookstore.Book", ReadTotalCount = true };
         var result = processingEngine.Execute(readCommandInfo);
+
         return $"{result.TotalCount} books.";
     }
 
@@ -32,4 +43,46 @@ public class DemoController : ControllerBase
         unitOfWork.CommitAndClose(); // Commits and closes database transaction.
         return "1 book inserted.";
     }
+
+    [HttpGet]
+    public IEnumerable GetListOfBooks()
+    {
+ 
+        return _context.Repository.Bookstore.Book.Load().ToList();
+    }
+
+    [HttpGet]
+    public IEnumerable GetBookAndAuthorLoad()
+    {
+
+        var list = _context.Repository.Bookstore.Book.Load().Select(book => book.Title).ToList();
+
+        return list;
+    }
+
+    [HttpGet]
+    public IEnumerable GetBookAndAuthorQuery()
+    {
+
+        var query = _context.Repository.Bookstore.Book.Query().Select(book => book.Title + '-' + book.Author.Name);
+        Console.WriteLine(query.ToString());
+
+        return query.ToList();
+    }
+
+
+    [HttpPost]
+    public void GenerateBooks(long numberOfBooks, string title)
+    { 
+
+        while(numberOfBooks > 0)
+        {
+
+            Random random = new Random();
+            _context.Repository.Bookstore.Book.Insert(new Book { Title = title, Code = random.Next(10000).ToString() });
+            --numberOfBooks;
+        }
+    }
+
+
 }
